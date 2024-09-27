de3d5d0c99448277fc9674e906d71e18072d129c
Pedro Gabriel Mandu dos Santos <105609774+pedrogabriel123@users.noreply.github.com>
2024-09-26 21:10:27 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+// Importa o componente LogoIcon do arquivo "./assets/icons".
 import { LogoIcon } from "./assets/icons"
+
+// Importa o componente CrudUser do caminho "./components/CrudUser".
 import CrudUser from "./components/CrudUser"
+
+// Importa o arquivo de estilos CSS principal da aplicação.
 import "./styles/App.css"
 
+// Define a função componente App, que é o componente principal da aplicação.
 function App() {
-	return (
-		<>
-			<header>
-				<div className='header__content'>
-					<div className='logo'>
-						<LogoIcon />
-						<strong>JSON SERVER API</strong>
-					</div>
-				</div>
-			</header>
-			<main>
-				<CrudUser />
-			</main>
-		</>
-	)
+  // Retorna o JSX que representa a interface do aplicativo.
+  return (
+    <>
+      {/* Cabeçalho da aplicação */}
+      <header>
+        <div className='header__content'>
+          <div className='logo'>
+            {/* Renderiza o ícone do logo */}
+            <LogoIcon />
+            {/* Exibe o título em negrito */}
+            <strong>JSON SERVER API</strong>
+          </div>
+        </div>
+      </header>
+      {/* Conteúdo principal da aplicação */}
+      <main>
+        {/* Renderiza o componente CrudUser, que gerencia as operações de CRUD de usuários */}
+        <CrudUser />
+      </main>
+    </>
+  )
 }
 
+// Exporta o componente App como a exportação padrão do módulo.
+// Isso permite que ele seja importado e utilizado em outros arquivos.
 export default App
