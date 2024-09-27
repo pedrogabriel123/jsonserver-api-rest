0478c414ac3e1b28d0802254486bf8a784e4b760
Pedro Gabriel Mandu dos Santos <105609774+pedrogabriel123@users.noreply.github.com>
2024-09-26 21:11:49 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+// Importa o React para utilizar funcionalidades de componentes.
 import React from "react";
+
+// Importa o ReactDOM para manipular o DOM com React.
 import ReactDOM from "react-dom";
+
+// Importa o componente App, que é o componente raiz da aplicação.
 import App from "./App";
 
+// Renderiza o componente App dentro do elemento com id 'root' no DOM.
 ReactDOM.render(
   <React.StrictMode>
     <App />
